defd3f58655556571ddc7779a5173e31ac2fb70a
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-06-27 02:58:07 -0300

    create database

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
+server.use(express.json());
+dotenv.config();
+
+const client = new MongoClient(process.env.URL_CONNECT_MONGO);
+let db;
+client.connect().then( ()=>{
+    db = client.db('batePapoUOL');
+});
+
+
+
+
+
+
 
 console.log("I'm run here");
-
-
-
-
-server.listen(5000);
+server.listen(5001);
