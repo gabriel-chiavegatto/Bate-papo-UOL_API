7018bf0757f2e09e4295880ae4afd0b33760e4b1
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-06-27 12:14:52 -0300

    user validation

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import { MongoClient } from "mongodb";
 import dotenv from 'dotenv';
 import cors from 'cors';
+import joi from 'joi';
+import dayjs from "dayjs";
 
 const server = express();
 server.use(cors());
@@ -10,21 +12,59 @@ server.use(express.json());
 dotenv.config();
 const client = new MongoClient(process.env.URL_CONNECT_MONGO);
 let db;
-client.connect().then( ()=>{
+client.connect().then(() => {
     db = client.db('batePapoUOL');
 });
 
-server.post('/participants', (req, res)=>{
-    console.log('passei polo post');
-    console.log(req.body);
-    db.collection('participants').insertOne(req.body);
+
+server.post('/participants', async (req, res) => {
+
+    const user = req.body;
+    console.log(user);
+    const userSchema = joi.object({
+        name: joi.string().required()
+    });
+    const validation = userSchema.validate(user);
+    console.log('sou a validation: ', validation)
+
+    if (validation.error) {
+        res.sendStatus(422);
+        return
+    }
+    const dateNow = Date.now();
+    console.log("datenow : ",dateNow);
+    const userLogin = {
+        name: user.name,
+        lastStatus: dateNow
+    }
+    const date = dayjs().format('HH:mm:ss');
+    console.log("horario: ",date)
+    const loginMessage = {
+        from: user.name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: date
+    }
+
+    try {
+        await db.collection('participants').insertOne(userLogin)
+        res.status(201).send();
+        await db.collection('messages').insertOne(loginMessage)
+    } catch (error) {
+        console.log('detalhes do erro: ', error);
+        res.sendStatus(422);
+    }
+
 });
-server.get('/participants', (req,res)=>{
+
+
+server.get('/participants', (req, res) => {
     console.log('tentando buscar as mensagens');
-    db.collection('partcipants').find().toArray().then(lista=>{
-        console.log('achei a colleçao, toma ai')
+    db.collection("participants").find().toArray().then(lista => {
+        console.log('achei a colleçao, toma ai', lista)
         res.status(200).send(lista);
-    })
+    });
 
 });
 
