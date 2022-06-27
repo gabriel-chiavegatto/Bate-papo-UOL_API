72bbd344b9f80ffb80233e6d6d8daee25f6e2fcb
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-06-27 04:29:18 -0300

    registration of participants

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ import cors from 'cors';
 const server = express();
 server.use(cors());
 server.use(express.json());
-dotenv.config();
 
+dotenv.config();
 const client = new MongoClient(process.env.URL_CONNECT_MONGO);
 let db;
 client.connect().then( ()=>{
     db = client.db('batePapoUOL');
 });
 
+server.post('/participants', (req, res)=>{
+    console.log('passei polo post');
+    console.log(req.body);
+    db.collection('participants').insertOne(req.body);
+});
+server.get('/participants', (req,res)=>{
+    console.log('tentando buscar as mensagens');
+    db.collection('partcipants').find().toArray().then(lista=>{
+        console.log('achei a colleçao, toma ai')
+        res.status(200).send(lista);
+    })
 
-
-
+});
 
 
 
