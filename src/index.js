26a8f9556a9d72420912008c2dedf07a0adfd322
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2022-09-05 02:55:36 -0300

    dotenv config and mongo connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import express, {json} from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
+import joi from "joi";
+import {MongoClient} from "mongodb";
 
 const server = express();
 server.use(json());
 server.use(cors());
+server.use(cors());
+dotenv.config();
+
+const cliente = new MongoClient(process.env.MONGO_URI);
+let db;
+cliente.connect().then(()=>{ db = cliente.db('batePapoUOL')});
 
 
 
-
-
-server.listen(5000, ()=>{console.log("Server ON")})
+server.listen(process.env.PORT, ()=>{console.log("Server ON")});
